989d0930ba1789abc426636c848852b8709cfcb9
Hsnmughal <hassanmughal12386@gmail.com>
2021-11-03 03:09:09 +0500

    started Todo

---
+++ src/components/TodoMap/TodoMapTemp.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './TodoMapTemp.module.css';
+
+const TodoMapTemp = () => {
+    return (
+        <div>
+            <span>
+                <p className={style.todo}>
+                    This is a todo
+                </p>
+            </span>
+            <button className={styles.edit}>
+                Edit
+            </button>
+            <button className={styles.del}>
+                Delete
+            </button>
+        </div>
+    )
+}
+
+export default TodoMapTemp
